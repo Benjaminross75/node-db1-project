11c01ac2221937e405966d650cf5c76e4c7ab983
Ben Ross <mrbenross@gmail.com>
2024-01-03 15:58:27 -0600

    new-commit

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -9,6 +9,12 @@ if(name === undefined || budget === undefined){
   res.status(400).json({message: "must be a string"})
 } else if(name.trim().length < 3 || name.trim().length > 100){
   res.status(400).json({message: "name of account must be between 3 and 100"})
+} else if(typeof budget !== 'number' || isNaN(budget)){
+  res.status(400).json({message: "budget of account must be a number"})
+} else if(budget < 0 || budget > 1000000){
+  res.status(400).json({message: "budget of account is too large or too small"})
+} else{
+  next()
 }
 }
 
